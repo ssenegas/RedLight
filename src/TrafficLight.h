58dc3d90377e1a4f1b61f228d42fcc5816255022
Sébastien Sénégas <ssenegas@gmail.com>
2023-09-29 00:07:55 +0200

    Cleanup.

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,26 +19,26 @@ class TrafficLight {
     void update(unsigned long currentTime);
 
 private:
-    void turnOnOff(Color c, bool value);
     void processLED(String command, Color c);
     
-private:
     const byte PIN_RED = 2;
     const byte PIN_YELLOW = 3;
     const byte PIN_GREEN = 4;
 
-    etl::map<Color, Led, 3> leds = etl::make_map<Color, Led>( etl::pair<Color, Led>{RED, Led(PIN_RED)},
-                                                              etl::pair<Color, Led>{YELLOW, Led(PIN_YELLOW)},
-                                                              etl::pair<Color, Led>{GREEN, Led(PIN_GREEN)} );
+    etl::map<Color, Led, 3> leds =
+      etl::make_map<Color, Led>( etl::pair<Color, Led>{RED, Led(PIN_RED)},
+                                 etl::pair<Color, Led>{YELLOW, Led(PIN_YELLOW)},
+                                 etl::pair<Color, Led>{GREEN, Led(PIN_GREEN)} );
 
     struct Status {
       char on;
       char off;
     };
 
-    etl::map<Color, Status, 3> status = etl::make_map<Color, Status>( etl::pair<Color, Status> {RED, {'R', 'r'}},
-                                                                      etl::pair<Color, Status> {YELLOW, {'Y', 'y'}},
-                                                                      etl::pair<Color, Status> {GREEN, {'G', 'g'}} );
+    etl::map<Color, Status, 3> status =
+      etl::make_map<Color, Status>( etl::pair<Color, Status> {RED, {'R', 'r'}},
+                                    etl::pair<Color, Status> {YELLOW, {'Y', 'y'}},
+                                    etl::pair<Color, Status> {GREEN, {'G', 'g'}} );
 };
 
 #endif
